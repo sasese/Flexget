06c5f6a98542e6b7fbd2f233028711c2bf4c844e
sasese <sasese@users.noreply.github.com>
2014-11-29 00:53:27 +0100

    Update cleanup_input.py

--- flexget/plugins/metainfo/cleanup_input.py
+++ flexget/plugins/metainfo/cleanup_input.py
@@ -9,12 +9,54 @@ log.verbose('!! cleanup_input !!')
 
 # AUTO-RUN CODE
     # Set priority to run plugin as the last in meta_input
-    # @plugin.priority(175)
-    # def on_task_filter(self, task, config):
+        # @plugin.priority(?)
+        # def on_task_filter(self, task, config):
 
-def CleanupInput(): # options: 'group' (no, quality, release), to do 'decrypt' (no, guess, strict)
+def CleanupInput():
 # SITUATION
     # Meta_input plugins have created fields.
+
+# OPTIONS
+    # 'group' (no, quality, release)
+    # Primary function, which sorts individual entries into group entries by:
+    # (general) quality, such as:
+        # entry: Birdwatcher Vigilante S01E01 x264
+            # (Birdwatching Vigilante S01E01 HDTV x264 - LOL)
+                 # URL 1: rapidgator.com/files/abc...
+                 # URL 2: rapidgator.com/files/def...
+                 # URL 3: ul.to/files/abc...
+                 # URL 4: netload.in/etc...
+            # (Birdwatching Vigilante S01E01 HDTV x264 - KiLLERS)
+                 # URL 5: etc...
+            # (Birdwatching Vigilante S01E01 HDTV x264 - FQM)
+                 # etc...
+    # or (exact) release:
+        # entry: Birdwatching Vigilante S01E01 HDTV x264 - LOL
+             # URL 1: rapidgator.com/files/abc...
+             # URL 2: rapidgator.com/files/def...
+             # URL 3: ul.to/files/abc...
+             # URL 4: netload.in/etc...
+        # entry: Birdwatching Vigilante S01E01 HDTV x264 - KiLLERS
+             # URL 1: etc...
+        # entry: Birdwatching Vigilante S01E01 HDTV x264 - FQM
+             # etc...
+    
+    # 'decypher' (to do) (no, guess, strict)
+        # Not yet implemented function, intended to obtain the names of files whose
+        # URLs are too cryptic to parse, such as uptobo.com/u73he7ehe82 instead of
+        # rapidgator.com/abc.../birdwatching.vigilante.s01e01.hdtv.x264.rar, either by
+            # guess, using modifications of the RSS and parse_urls plugins to add the
+            # name of originating items, thus providing clues, or;
+            # strict(ly), parsing an entry's URL directly or querying it via a site's API. 
+    
+    schema = {
+        'type': ['string', 'object'],
+        'properties': {
+            'group': {'type': 'string'},
+            'decypher': {'type': 'string'}
+        },
+        'required': ['group']
+    }
 
 # INITIALIZE
     # Copy task.entries to cleanup.entries
