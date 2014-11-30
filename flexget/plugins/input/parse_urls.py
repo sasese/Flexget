16f6df5a16347025ed382c92f3250d3a18152223
sasese <sasese@users.noreply.github.com>
2014-11-30 17:41:41 +0100

    Inclusion of pyLoad API module's ParseURL

--- flexget/plugins/input/parse_urls.py
+++ flexget/plugins/input/parse_urls.py
@@ -6,6 +6,8 @@ from urlparse import urlparse
 from flexget import plugin
 from flexget.event import event
 
+urlmatcher = re.compile(r"((https?|ftps?|xdcc|sftp):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+\-=\\\.&]*)", re.IGNORECASE)
+
 log = logging.getLogger('!! parse_urls !!')
 log.verbose('!! parse_urls !!') 
 
@@ -20,22 +22,47 @@ def ParseURLs(): # options: none, for now
 
 # INITIALIZE
     # Copy task.entries to parse.entries
-    parse.entries = copy.deepcopy(task.entries)
+    parse.entries = task.entries
     # Reset task.entries
     task.entries.clear()
 
 # MAIN
     # for each entry in parse.entries
-    for each entry in parse.entries
-        # for each url in entry
+    for entry in parse.entries:
+        # for each url in entry (entry['url'])
+        for url in entry['url']:
+        # D:\pp\App\Lib\site-packages\flexget\plugins\filter\magnets.py
+        # 18: entry['urls'] = [url for url in entry['urls'] if not url.startswith('magnet:')]
             # parse url into parsed_urls
+                # inspect pyload plugin for method
             # for each url in parsed_urls
                 # parse name from url
                 # create task.entry [name, url]
 
 # CONCLUDE
     # Clean-up
+    
+def parseURLs(self, html=None, url=None):
+    """
+    Copied from pyLoad API module's parseURLs
+    Parses html content or any arbitaty text for links and returns result of `checkURLs`
+    
+    :param html: html source
+    :return:
+    """
+    
+    urls = []
+    
+    if html:
+        urls += [x[0] for x in urlmatcher.findall(html)]
         
+    if url:
+        page = getURL(url)
+        urls += [x[0] for x in urlmatcher.findall(page)]
+        
+    # remove duplicates
+    return self.checkURLs(set(urls))
+
 
     @event('plugin.register')
     def register_plugin():
